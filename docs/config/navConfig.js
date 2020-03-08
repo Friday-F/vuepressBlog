3ccd5174a28459ddbb0f014613a6935c94a7260b
crayon <2545497785@qq.com>
2020-03-08 19:13:25 +0800

    link-edit

--- docs/config/navConfig.js
+++ docs/config/navConfig.js
@@ -1,5 +1,5 @@
 module.exports = [ // 导航栏配置
-    {text: '面试', link: '/interview/'},
+    {text: '面试', link: '/interview/javascript/prototype'},
     {text: '随笔', link: '/accumulate/' },
     {text: '杂七', link: 'https://baidu.com'}      
 ]
