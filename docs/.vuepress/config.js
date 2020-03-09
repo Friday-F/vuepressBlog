403714953cb3553a8fc7ea669050d9f753c161e3
crayon <2545497785@qq.com>
2020-03-09 10:57:10 +0800

    icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,8 +4,7 @@ module.exports = {
     title: '前端之路',
     description: '前端*前端',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
-      ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
-      ["link", { rel: "stylesheet", href: "/style/reset.less" }],
+      ['link', { rel: 'icon', href: './favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
