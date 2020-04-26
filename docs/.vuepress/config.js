d4b4f443af00c064014011d3b27f7e16d83dd4b7
crayon <2545497785@qq.com>
2020-04-26 22:28:12 +0800

    ico

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,10 @@
 const navConfig = require('../config/navConfig');
 const sidebarConfig = require('../config/sidebarConfig');
 module.exports = {
-    title: '前端之路',
+    title: '蜡笔小新',
     description: '前端*前端',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
-      ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+      ['link', { rel: 'shortcut icon',type: "image/x-icon", href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
